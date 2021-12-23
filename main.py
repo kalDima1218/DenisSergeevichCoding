1144a8737aaccc9e192e5426d1aea8c6f5042b43
kalDima1218 <kalugin180806@gmail.com>
2021-12-23 19:21:31 +0300

    ДСП на троичном дереве

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from random import randint
 
-#Исходное сообщение
+# Исходное сообщение
 s = "Здравствуйте Денис Сергеевич! Могу вас заверить, что новый год буду встречать в маске и перчатках."
 
-#Расстановка символов в вершинах дерева
+# Расстановка символов в вершинах дерева
 chars = list(set(s))
 eq = 1
 while eq == 1:
@@ -14,7 +14,7 @@ while eq == 1:
         if ways[i] == ways[i + 1]:
             eq = 1
 
-#Генерация ключей
+# Генерация ключей
 char_num = {}
 num_char = {}
 for i in range(len(chars)):
@@ -23,7 +23,7 @@ for i in range(len(chars)):
         char_num[chars[i]] += str(j)
     num_char[char_num[chars[i]]] = chars[i]
 
-#Шифрование
+# Шифрование
 encrypted = ""
 for i in s:
     encrypted += char_num[i]
@@ -34,7 +34,7 @@ encrypted = encrypted.replace("2", "П")
 
 print(encrypted)
 
-#Расшифровка
+# Расшифровка
 encrypted = encrypted.replace("Д", "0")
 encrypted = encrypted.replace("С", "1")
 encrypted = encrypted.replace("П", "2")
@@ -43,12 +43,12 @@ decrypted = ""
 l = 0
 r = 1
 
-while(r <= len(encrypted)):
+while r <= len(encrypted):
     try:
         decrypted += num_char[encrypted[l:r]]
         l = r
     except:
         pass
-    r+=1
+    r += 1
 
 print(decrypted)
